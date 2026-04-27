678bc77a26e8b4c0b7e47583188f79dcda2f92c4
Student <foobar@sr.htlweiz.at>
2026-04-27 21:14:25 +0200

    tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -36,7 +36,8 @@ public class BoardTests
     }
 
     [Fact]
-    public void Setup_Rook1IstPlaced(){
+    public void Setup_Rook1IstPlaced()
+    {
 
         Board board = new Board();
 
@@ -45,8 +46,9 @@ public class BoardTests
         Assert.Contains("R", output);
     }
 
-        [Fact]
-    public void Setup_Rook2IstPlaced(){
+    [Fact]
+    public void Setup_Rook2IstPlaced()
+    {
         Board board = new Board();
 
         string output = board.ToString();
@@ -78,12 +80,49 @@ public class BoardTests
         Assert.Contains("R", result);
     }
 
-        [Fact]
+    [Fact]
     public void MoveFigure_Rook2Moves()
     {
         Board board = new Board();
 
         board.MoveFigure("Rook2", 7, 3);
+
+        string result = board.ToString();
+
+        Assert.Contains("R", result);
+    }
+
+    [Fact]
+    public void MoveFigure_KingmvesOneStep()
+    {
+        Board board = new Board();
+
+        board.MoveFigure("King", 5, 1);
+
+        string result = board.ToString();
+
+        Assert.Contains("K", result);
+    }
+
+
+    [Fact]
+    public void MoveFigure_Rook_1_and_2_MoveHorizontaly()
+    {
+        Board board = new Board();
+
+        board.MoveFigure("Rook1", 3, 0);
+
+        string result = board.ToString();
+
+        Assert.Contains("R", result);
+    }
+
+    [Fact]
+    public void MoveFigure_RookMovesVertical()
+    {
+        Board board = new Board();
+
+        board.MoveFigure("Rook1", 0, 4);
 
         string result = board.ToString();
 
